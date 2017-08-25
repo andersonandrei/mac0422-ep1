3b26e40c5a4e393fad28bfccb8dd5e73abbfeb9e
Andrei <a.andreisilva@yahoo.com.br>
2017-08-25 19:17:12 -0300

    fork and execv to (ping and cal) done.

--- ep1sh.c
+++ ep1sh.c
@@ -15,7 +15,6 @@ typedef struct {
   int qnt;
 } input;
 
-
 void mysplit(char *in, input *inp) {
   int i, n = 0;
   for (i = 0; i<strlen(in) ; i++) {
@@ -24,66 +23,48 @@ void mysplit(char *in, input *inp) {
     }
   }
   inp->qnt = n+2;
-  inp->text = malloc (n+2 * sizeof(char *));
-  inp->text[0] = malloc (50 * sizeof(char));
+  inp->text = malloc ((n+2) * sizeof(char *));
   inp->text[0] = strtok(in, " ");
   for(i = 1; i < n+1; i++) {
-    inp->text[i] = malloc (50 * sizeof(char));
     inp->text[i] = strtok(NULL, " ");
   }
   inp->text[n+1] = 0; 
   return;
 }
 
-void calls (input *inp) {
+void calls (input inp) {
   int i;
-  printf("Aqui: %s", inp->text[0]);
-  printf("\n Merda -----------");
-  for(i = 0; i < inp->qnt; i++){
-    printf("\n %s",inp->text[i]);
+  if (strcmp(inp.text[0],"/bin/ping") == 0 || strcmp(inp.text[0],"/usr/bin/cal") == 0 || strcmp(inp.text[0],"./ep1") == 0) {
+    printf("%d",execv(inp.text[0], inp.text));
   }
-  //if (strcmp(inp->text[0],"/bin/ping") == 0 || strcmp(inp->text[0],"/usr/bin/cal") == 0 || strcmp(inp->text[0],"./ep1") == 0) {
-  printf("%d",execv(inp->text[0], inp->text));
-  //}
-  //else {
-  /*  printf("Command not found");
-  }*/ 
+  else {
+    printf("Command not found");
+  } 
 }
 
 int main ()
 {
-  input *inp;
+  input inp;
   char *in; 
   int ret, i;
   while(1) {
     in = malloc (sizeof (char *));
-    inp = malloc (sizeof (input));
     in = readline("\n[./]$ ");
-    mysplit(in, inp);
+    mysplit(in, &inp);
     ret = fork();
     if(ret == 0)
     {
       //child process
-      printf("\n porra");
       calls(inp);
-      printf("\n caralho");
     }
     else if (ret == -1) {
       printf("forkFailed\n");
     }
     else
     {
-       //you will come here only if fork() fails.
        waitpid(ret, NULL, 0);
-       
     }
-/*    for (i = 0; i < inp->qnt; i++) {
-      printf("\n freezou %d",i);
-      free(inp->text[i]);
-    }*/
-    free(inp);
-    /*free(inp);
-    */
+    free(inp.text);
     free(in);
   }
   return 0;
