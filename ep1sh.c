f54cda4bc9c3843edde8652a2224a01d07227ae6
Andrei <a.andreisilva@yahoo.com.br>
2017-08-31 21:02:21 -0300

    Commit the functions and organizing the code.

--- ep1sh.c
+++ ep1sh.c
@@ -10,17 +10,20 @@
 #include <readline/history.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <errno.h>
-
 #include <sys/types.h>
 #include <grp.h>
+#include <time.h>
 
 
 typedef struct {
   char **text;
   int qnt;
 } input;
+
+/* Receive a struct input and a readline 'in',
+   split the 'in' and save in the input table
+   of words, couting this. */
 
 void mysplit(char *in, input *inp) {
   int i, n = 0;
@@ -39,6 +42,10 @@ void mysplit(char *in, input *inp) {
   return;
 }
 
+/* Receive the input, and do the external
+   call, using the fork() and execve().
+   Here, our implementation do any external
+   call (that use fork and execve). */
 void externalCalls (input inp) {
   int ret;
   ret = fork();
@@ -52,27 +59,52 @@ void externalCalls (input inp) {
      waitpid(ret, NULL, 0);
   }
 }
-   
-void internCalls (input inp) {
-  int uid = getuid();
-  char *gp;
 
+/* Receive the input, verify what intern
+   call should be executed, and invoke it. */
+void internCalls (input inp) {
+  if (strcmp(inp.text[0],"chown") == 0) {
+    myChown(inp);
+  }
+  else if (strcmp(inp.text[0],"date") == 0) {
+    date(inp);
+  }
+}
+
+
+/* Receive the input and execute 
+   our implementation of chown function. */
+void myChown (input inp) {
+  int uid = getuid();
+  struct group *gr;
+
+  gr = getgrnam(inp.text[1]+1);
+  chown(inp.text[2], uid, gr->gr_gid);
+}
+
+/* Receive the input and execute our implementation
+   of date function using strftime funtion.
+   For this :
+    we include : #include <time.h>
+    and use: %a %b %d %I:%M:%S %Z %Y
+      for repectively day of week, month, day (decimal),
+      hours, minutes, seconds, time zone and year. */
+void myDate (input inp) {
   time_t t = time(NULL);
   char dateBuffer[80];
   struct tm *tm = localtime(&t);
   time(&t);
   tm = localtime(&t);
 
-  if (strcmp(inp.text[0],"chown") == 0) {
-    gr = getgrnam(inp.text[1]+1);
-    chown(inp.text[2], uid, gr->gr_gid);
-  }
-  else if (strcmp(inp.text[0],"date") == 0) {
-    strftime(dateBuffer,80,"%a %b %d %I:%M:%S %Z %Y", tm);
-    printf("%s\n", dateBuffer);
-  }
+  strftime(dateBuffer,80,"%a %b %d %I:%M:%S %Z %Y", tm);
+  printf("%s\n", dateBuffer);
 }
 
+/* Receive the input, verify if the call
+   required is valid in our shell and
+   verify what kind of call is it, to 
+   do the call (external or intern).
+*/
 void calls (input inp) {
   if (strcmp(inp.text[0],"/bin/ping") == 0 || strcmp(inp.text[0],"/usr/bin/cal") == 0 || strcmp(inp.text[0],"./ep1") == 0) {
     externalCalls(inp);
